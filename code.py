6a9fcc03361469df8d3c2e90755dac919373de74
Eric Z. Ayers <ericzundel@gmail.com>
2025-04-23 12:51:11 -0400

    Example for playing a .mp3 file

--- code.py
+++ code.py
@@ -8,7 +8,10 @@
 # See README.md for notes.
 
 import board
-import audiocore
+
+import audiocore   # To play a .wav file
+import audiomp3    # To play an mp3 file
+
 import audiobusio
 from digitalio import DigitalInOut, Direction, Pull
 
@@ -19,8 +22,11 @@ button.pull = Pull.UP
 
 # Wav file must be 16bit, <= 22 Khz. You can convert your file using the Audacity app
 # See also https://learn.adafruit.com/adafruit-wave-shield-audio-shield-for-arduino/convert-files
-wave_file = open("batman_theme_x_11khz.wav", "rb")
-wave = audiocore.WaveFile(wave_file)
+#wave_file = open("batman_theme_x_11khz.wav", "rb")
+#sound_clip = audiocore.WaveFile(wave_file)
+
+# Here's how you play an mp3 file.  Much smaller than a .wav file!
+sound_clip = audiomp3.MP3Decoder("mario_11khz.mp3")
 
 # For Raspberry Pi Pico I used GP2 = data(DIN), GP3 = bit clock(BCLK), GP4 = word select (LRC)
 audio = audiobusio.I2SOut(board.GP3, board.GP4, board.GP2)
@@ -32,7 +38,7 @@ while True:
     if (button.value == False) :
         # Each time you call audio.play() the currently playing sound clip
         # will stop and it will start playing the new sound.
-        audio.play(wave)
+        audio.play(sound_clip)
 
         # This loop waits until the audio is finished playing.
         # The entire program pauses during this loop, so you might
